f714105636c69005a3f983fd7c5f762a4657608d
Randall McPherson <rlmcpherson@gmail.com>
2015-10-05 21:57:29 -0400

    max url length of 3000 chars

--- internal/database/pg.sql
+++ internal/database/pg.sql
@@ -1,6 +1,6 @@
 CREATE TABLE urls
 (
         short character varying(20) NOT NULL PRIMARY KEY,
-        url character varying(255) NOT NULL,
+        url character varying(3000) NOT NULL,
         created date not null default CURRENT_DATE
 );
